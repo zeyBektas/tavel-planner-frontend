63db7f7037bf2979e5af1374efbffd92d0741fa9
zeynep <zeynep203551@hotmail.com>
2024-07-08 12:05:19 +0300

    fix datepicker

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
 import { LandingComponent } from './Pages/landing/landing.component';
 import { TripPlannerComponent } from './Pages/trip-planner/trip-planner.component';
+import { PlacesComponent } from './Pages/places/places.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'login', component: LoginComponent },
     { path: 'landing', component: LandingComponent },
-    { path: 'trip-planner', component: TripPlannerComponent}
+    { path: 'trip-planner', component: TripPlannerComponent},
+    { path: 'places', component: PlacesComponent }
 ];
