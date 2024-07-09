e518d8041151b9864069a1b34fa9908c81973e35
OnurArslan0 <onursln11@gmail.com>
2024-07-10 00:14:54 +0300

    add route for about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './Pages/login/login.component';
 import { LandingComponent } from './Pages/landing/landing.component';
 import { TripPlannerComponent } from './Pages/trip-planner/trip-planner.component';
 import { PlacesComponent } from './Pages/places/places.component';
+import { AboutUsComponent} from './Pages/about-us/about-us.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
@@ -10,4 +11,5 @@ export const routes: Routes = [
     { path: 'landing', component: LandingComponent },
     { path: 'trip-planner', component: TripPlannerComponent},
     { path: 'places', component: PlacesComponent },
+    { path: 'about-us', component: AboutUsComponent}
 ];
