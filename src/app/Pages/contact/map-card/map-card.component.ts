1c5976dbfd3d37b14955190c84740ef46a9fef9a
OnurArslan0 <onursln11@gmail.com>
2024-07-10 11:16:56 +0300

    contact and about us pages

---
+++ src/app/Pages/contact/map-card/map-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-card.component.html',
+  styleUrl: './map-card.component.scss'
+})
+export class MapCardComponent {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LandingComponent } from './Pages/landing/landing.component';
 import { TripPlannerComponent } from './Pages/trip-planner/trip-planner.component';
 import { PlacesComponent } from './Pages/places/places.component';
 import { AboutUsComponent} from './Pages/about-us/about-us.component';
+import { ContactComponent } from './Pages/contact/contact-page/contact.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'landing', component: LandingComponent },
     { path: 'trip-planner', component: TripPlannerComponent},
     { path: 'places', component: PlacesComponent },
-    { path: 'about-us', component: AboutUsComponent}
+    { path: 'about-us', component: AboutUsComponent},
+    { path: 'contact', component: ContactComponent}
 ];
