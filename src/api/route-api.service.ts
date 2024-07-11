08a2ab6955fdd00f06e081db59c564ce6a20f400
zeynep <zeynep203551@hotmail.com>
2024-07-11 10:46:51 +0300

    all routes api

--- src/api/route-api.service.ts
+++ src/api/route-api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteApiService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  getAllRoutes(userId: string): Observable<any> {
+    return this.http.get(`${environment.BASE_URL}route/getRoutes/${userId}`);
+  }
+
+  cancelRoute(routeId: string): Observable<any> {
+    return this.http.post(`${environment.BASE_URL}route/cancelRoute`, routeId);
+  }
+
+}  
--- src/app/Pages/all-trips/all-trips.component.ts
+++ src/app/Pages/all-trips/all-trips.component.ts
@@ -1,23 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { RouteFacadeService } from '../../../façade/route-facade.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ModalComponent } from '../../Layouts/modal/modal.component';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-all-trips',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, JsonPipe, MatDialogModule],
   templateUrl: './all-trips.component.html',
   styleUrl: './all-trips.component.scss'
 })
 export class AllTripsComponent implements OnInit {
 
-  constructor() { }
+  routeFacadeService = inject(RouteFacadeService);
+  dialog = inject(MatDialog);
+
+  constructor() {}
 
   ngOnInit(): void {
-    
+    this.getRoutes();
   }
 
-  openPopup() {
-    alert("Popup");
+  getRoutes() {
+    this.routeFacadeService.getAllRoutes(localStorage.getItem('userId') || '');
+    console.log('my routesssssss');
+  }
+
+  openPopup(tripId: string) {
+    const dialogRef = this.dialog.open(ModalComponent, {
+      width: '250px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.routeFacadeService.cancelRoute(tripId);
+      }
+    });
   }
 }
-
